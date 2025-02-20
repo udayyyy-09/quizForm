580c8feb23dc2a4075fbb4864285b720483f0c3a
udayyyy-09 <164606930+udayyyy-09@users.noreply.github.com>
2025-02-20 20:32:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
 import QuizPlatform from './components/QuizPlatform';
 function App() {
-
   return (
-    <>
-        <QuizPlatform/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/quiz" element={<QuizPlatform />} /> 
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
